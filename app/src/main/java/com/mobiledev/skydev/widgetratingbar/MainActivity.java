afdaa4a57147ab852da669cd224ceaff0cacb491
guapusing <rendisukirman.rs@gmail.com>
2018-01-04 21:57:38 +0700

    buat ratting

---
+++ app/src/main/java/com/mobiledev/skydev/widgetratingbar/MainActivity.java
@@ -0,0 +1,35 @@
+package com.mobiledev.skydev.widgetratingbar;
+
+import android.view.View.OnClickListener;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RatingBar;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    RatingBar ratingbar1;
+    Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        addListenerOnButtonClick();
+    }
+
+    private void addListenerOnButtonClick() {
+        ratingbar1=(RatingBar)findViewById(R.id.ratingbar1);
+        button=(Button)findViewById(R.id.button1);
+        //Performing action on Button Click
+        button.setOnClickListener(new OnClickListener(){
+
+            @Override
+            public void onClick(View arg0) {
+                //Getting the rating and displaying it on the toast
+                String rating=String.valueOf(ratingbar1.getRating());
+                Toast.makeText(getApplicationContext(), rating, Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
